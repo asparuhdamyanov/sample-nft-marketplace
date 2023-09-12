4b86f09b0a36a82c5be1754c867b1ebd141804c0
Asparuh <asparuhdamyanov@gmail.com>
2023-09-12 13:10:55 +0300

    update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,14 @@
-require('@nomiclabs/hardhat-waffle');
-require('@nomiclabs/hardhat-etherscan');
-require('dotenv').config();
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
-const { RINKEBY_API_URL, PRIVATE_KEY, ETHERSCAN_KEY } = process.env;
+const { GOERLI_API_URL, PRIVATE_KEY, ETHERSCAN_KEY } = process.env;
 
 module.exports = {
-  solidity: '0.8.4',
+  solidity: "0.8.4",
   networks: {
-    rinkeby: {
-      url: RINKEBY_API_URL,
+    goerli: {
+      url: GOERLI_API_URL,
       accounts: [`0x${PRIVATE_KEY}`],
     },
   },
